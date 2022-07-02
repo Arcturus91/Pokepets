6cbe13bdc8856cd3c7fa813b7a0f710587c8cdd4
Ulises-lab <yaftemiranda@gmail.com>
2022-07-02 15:03:16 -0500

    done session persistente

--- routes/authUser.routes.js
+++ routes/authUser.routes.js
@@ -2,10 +2,18 @@ const router = require('express').Router();
 const User = require('../models/User.model');
 const bcryptjs = require('bcryptjs');
 
+
 router.get('/signup',(req,res,next)=>{
-    console.log('Llegaste a la ruta de logueo');
-    res.render('auth/userSignup');
-})
+    console.log('Llegaste a la ruta de logueo')
+    console.log('El req.session', req.session);
+    if (req.session.currentUser) {
+        console.log("req.session.currentUser: ", req.session.currentUser)
+        return res.redirect(`user/userProfile/${req.session.currentUser._id}`)
+      }
+    //console.log("req.session.currentUser: ", req.session.currentUser)
+
+    res.render('auth/userSignup')
+});
 
 router.post('/signup',(req,res,next)=>{
     const {username,lastname,password,number,email,profile_pic} = req.body;
@@ -23,7 +31,10 @@ router.post('/signup',(req,res,next)=>{
         })
     .then(userFromDB =>{
         console.log('New user create', userFromDB)
+        req.session.currentUser = userFromDB
+        console.log('El req.session: ', req.session);
         res.redirect(`user/userProfile/${userFromDB._id}`)
+        
     })
     .catch(error =>{
         console.log('Ha salido un error en el post ID',error)
@@ -35,13 +46,37 @@ router.get('/user/userProfile/:id',(req,res,next)=>{
     const {id} = req.params;
     console.log('Llegaste al get de userProfile')
     User.findById(id)
-    .then(username =>{
-        res.render('user/userProfile',username)
+    .then(user =>{
+        res.render('user/userProfile',{user})
     })
     .catch(error =>{
         console.log('Ha salido un error en el get ID',error)
         next(error)
     })
+})
+
+router.get('/login',(req,res,next)=>{
+    res.render('auth/userLogin')
+})
+
+router.post('/login', (req,res,next)=>{
+    const {username, password} = req.body;
+    if(!username||!password){
+        res.render('auth/userLogin',{
+            errorMessage:'ingresa username y password'
+        });
+        return;
+    }
+    user.findOne({username})
+    .then(user =>{
+        if(!user){
+            res.render('auth/userLogin')
+            return
+        }else if(bcryptjs.compareSync(password,user.password)){
+            res.render('/user/userProfile')
+        }
+    })
+
 })
 
 module.exports = router;
